f80cb439eb107de1c309899fdc66573d9dca5506
Fullfifax <fullfifax1245@gmail.com>
2022-12-20 11:44:17 +0000

    add route & create AboutController & extend layout

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron mt-3">
+    <div class="container">
+        <h1 class="display-3 text-secondary">Kotrana Laravel :)</h1>
+        <p>Simple todo list using laravel ;)</p>        
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AboutController::class, 'index'])->name('about');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); 
